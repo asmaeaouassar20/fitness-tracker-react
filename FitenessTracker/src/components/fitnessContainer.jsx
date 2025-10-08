c6af69f59d48222b82cf10a3574bfc7153f4b335
asmae <asmaeaouassar333@example.com>
2025-10-08 20:46:19 +0100

    L'action ajouter permet d'ajouter une nouvelle tâche dans la liste des tâches

--- FitenessTracker/src/components/fitnessContainer.jsx
+++ FitenessTracker/src/components/fitnessContainer.jsx
@@ -2,14 +2,40 @@ import { Header } from "./header/header";
 import { FitnessInput } from "./fitnessInput/fitnessInput";
 import {FitnessList} from "./fitnessList/fitnessList";
 import { Footer } from "./footer/footer";
+import { useState } from "react";
 
 export const FitnessContainer = () => {
+
+    const [tasksList, setTasksList] = useState([
+        {
+            id : 1,
+            title : "Tâche initiale pour testing",
+            completed : false,
+        }
+    ]);
+
+    console.log(tasksList);
+
+    const addTask = (title) => {
+        const newTask = {
+            id : tasksList.length + 1,
+            title : title,
+            completed : false
+        };
+        setTasksList([...tasksList, newTask]); // l’opérateur spread ...
+    }
+
     return (
         <main>
             <Header />
-            <FitnessInput />
+            <FitnessInput addTask={addTask} /> 
             <FitnessList />
             <Footer/>
         </main>
     )
 }
+
+
+
+
+
--- FitenessTracker/src/components/fitnessInput/fitnessInput.jsx
+++ FitenessTracker/src/components/fitnessInput/fitnessInput.jsx
@@ -1,16 +1,32 @@
 // ce composant sera utilisé pour afficher le champ de saisie de l'exercice à faire
+import { useState } from 'react';
 import styles from './FitnessInput.module.css';
-export const FitnessInput = () => {
+export const FitnessInput = ({ addTask }) => {
+
+    const [taskTitle,setTaskTitle] = useState("");
+  
+
+    const handleInputChange = (e) => {
+        setTaskTitle(e.target.value);
+    };
+    const handleAddTask = (e) => {
+        e.preventDefault();
+        addTask(taskTitle);
+        setTaskTitle("");
+    }
+
     return (
         <div className={`box ${styles.element}`}>
             <h2>Ajoute ta prochaine activité</h2>
-            <form className={styles.container}>
+            <form className={styles.container} onSubmit={handleAddTask}>
                 <input
                     type="text"
                     className={styles.input}
                     placeholder="Indiquez un titre pour votre activité"
+                    onChange={handleInputChange}
+                    value={taskTitle}
                 />
-                <button className={styles.addBtn}>Ajouter</button>
+                <button className={styles.addBtn} type='submit'>Ajouter</button>
             </form>
             
         </div>
